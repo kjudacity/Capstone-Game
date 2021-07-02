5f1f2dfc70d41aca29bcdac6c7ff6f5a51b54ecb
kjudacity <85656781+kjudacity@users.noreply.github.com>
2021-07-01 21:44:09 -0400

    Update Board.cpp

--- src/Board.cpp
+++ src/Board.cpp
@@ -4,6 +4,19 @@
 #include <sstream>
 #include "level.h"
 
+Rail::Rail(int init_x1, int init_y1, int init_x2, int init_y2) {
+	x1 = init_x1;
+	y1 = init_y1;
+	x2 = init_x2;
+	y2 = init_y2;
+	for (int i=0; i<12; i++)
+		idxPills[i] = -1;
+	numPills = 0;
+}
+
+Rail::~Rail() {
+}
+
 Board *Board::instance = NULL;
 
 Board *Board::getInstance() {
